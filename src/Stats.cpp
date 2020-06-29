38e973d74e2e038418e1cb5a9d2e12c5d37f6cbe
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-29 19:14:12 +0200

    Compute expected runtime and performance

--- src/Stats.cpp
+++ src/Stats.cpp
@@ -1,5 +1,36 @@
+#include <iostream>
 #include "NBody.h"
 
+void PrintUsage() {
+  std::cerr << "Usage: ./Stats timesteps [<routed frequency>]\n" << std::flush;
+}
+
+// Prints the expected performance of the current configuration in hardware.
+// If a different frequency is achieved, it can be passed as the first argument
+// to the executable.
 int main(int argc, char **argv) {
+  if (argc > 3 || argc < 2) {
+    PrintUsage();
+    return 1;
+  }
+  const auto timesteps = std::stoul(argv[1]);
+  const float frequency = (argc < 3) ? kFrequency : std::stof(argv[2]);
+  const unsigned long long nOps =
+      22 * kNBodies * kNBodies * timesteps + 12 * timesteps;
+  std::cout << "Frequency:            " << frequency << " MHz\n";
+  std::cout << "Number of operations: " << nOps << " ("
+            << static_cast<float>(nOps) << ")\n";
+  const auto expected_runtime = timesteps *
+                                (kUnrollDepth * kPipelineFactor +
+                                 kNTiles * (kNBodies * kPipelineFactor +
+                                            kUnrollDepth * kPipelineFactor)) /
+                                (1e6 * frequency);
+  const auto expected_perf = 1e-9 * nOps / expected_runtime;
+  const auto peak_perf = 1e-9 * (kUnrollDepth * 22 * (1e6 * frequency));
+  std::cout << "Expected runtime:     " << expected_runtime << " seconds\n";
+  std::cout << "Peak runtime:         " << nOps / (1e9 * peak_perf)
+            << " seconds\n";
+  std::cout << "Expected performance: " << expected_perf << " GOp/s\n";
+  std::cout << "Peak performance:     " << peak_perf << " GOp/s\n";
   return 0;
 }
